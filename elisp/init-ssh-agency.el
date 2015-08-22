2d851e4fb3cee39bf8a087a1ac1177c68976f4eb
b54898533 <b54898533@gmail.com>
2015-08-22 20:56:00 +0800

    modify elisp/init-ssh-agency.el

--- elisp/init-ssh-agency.el
+++ elisp/init-ssh-agency.el
@@ -1,2 +1,3 @@
 
-'(ssh-agency-add-executable "d:/Cheng/Program/tool/Git/bin/usr/ssh-add.exe")
+(eval-after-load "ssh-agency"
+  '(ssh-agency-add-executable "d:/Cheng/Program/tool/Git/bin/usr/ssh-add.exe"))
